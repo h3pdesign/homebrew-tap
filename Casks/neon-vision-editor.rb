fcafd2880a4945da38615fefece5e15ca9a3cb1b
h3p <apps@hpedersen.de>
2026-02-08 23:53:00 +0100

    Add cask and auto-update workflow

---
+++ Casks/neon-vision-editor.rb
@@ -0,0 +1,11 @@
+cask "neon-vision-editor" do
+  version "0.4.2-beta"
+  sha256 "aa832ffecd207ebd869ac16d61e3cd3d741eda051f2456ee5a7215e46fed8431"
+
+  url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
+  name "Neon Vision Editor"
+  desc "Lightweight, modern editor focused on speed and readability"
+  homepage "https://github.com/h3pdesign/Neon-Vision-Editor"
+
+  app "Neon Vision Editor.app"
+end
---
+++ README.md
@@ -0,0 +1,6 @@
+# h3pdesign/homebrew-tap
+
+```bash
+brew tap h3pdesign/tap
+brew install --cask neon-vision-editor
+```
