7c613948535ddaf28b024d4c3dd0e7cb58a0dc17
h3p <apps@hpedersen.de>
2026-02-09 00:06:42 +0100

    Fix sha256 for v0.4.3-beta

--- Casks/neon-vision-editor.rb
+++ Casks/neon-vision-editor.rb
@@ -1,6 +1,6 @@
 cask "neon-vision-editor" do
   version "0.4.3-beta"
-sha256 "NEW_SHA256"
+sha256 "d615e4302aa5137e084ff2835badd3280cad8bfba9bb410d80b2bac99f092737"
 
   url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
   name "Neon Vision Editor"
