188c6fdad23deaa33394c89b3be19df58d166fb0
h3p <apps@hpedersen.de>
2026-02-11 14:13:56 +0100

    Update neon-vision-editor cask SHA for v0.4.5 asset

--- Casks/neon-vision-editor.rb
+++ Casks/neon-vision-editor.rb
@@ -1,8 +1,8 @@
 cask "neon-vision-editor" do
-  version ""
-  sha256 ""
+  version "0.4.5"
+  sha256 "5a78b7d562729c2c3cfaaa39618b0611334d338e82ddc7dd394acb54f7253f8e"
 
-  url ""
+  url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
   name "Neon Vision Editor"
   desc "Lightweight, modern editor focused on speed and readability"
   homepage "https://github.com/h3pdesign/Neon-Vision-Editor"
