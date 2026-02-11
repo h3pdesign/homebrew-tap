1f6e5ef5454fb2936dbdde121b73d4eb08c78bd5
h3p <apps@hpedersen.de>
2026-02-11 11:54:40 +0100

    cask: bump neon-vision-editor to v0.4.5

--- Casks/neon-vision-editor.rb
+++ Casks/neon-vision-editor.rb
@@ -1,8 +1,8 @@
 cask "neon-vision-editor" do
-  version ""
-sha256 ""
+  version "0.4.5"
+  sha256 "eca61586454f6600f4cb44c2b0bb340b2dfa2042150e078d8402dd24b1c2be0d"
 
-  url ""
+  url "https://github.com/h3pdesign/Neon-Vision-Editor/releases/download/v#{version}/Neon.Vision.Editor.app.zip"
   name "Neon Vision Editor"
   desc "Lightweight, modern editor focused on speed and readability"
   homepage "https://github.com/h3pdesign/Neon-Vision-Editor"
